d31cb62a5bb4d988b9885048e943d9b3d275d0c3
Philippe M. Chiasson <gozer@mozilla.com>
2015-01-10 14:50:14 -0500

    add puppet stub

---
+++ packer/puppet.pp
@@ -0,0 +1,15 @@
+package { "jenkins":
+    ensure => "1.596",
+}
+
+package { "unzip":
+    ensure => "6.0-9ubuntu1",
+}
+
+package { "git":
+    ensure => "1:1.9.1-1",
+}
+
+package { "make":
+    ensure => "3.81-8.2ubuntu3",
+}
