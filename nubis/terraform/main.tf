efba61df872157e870389a905de8f1f11f1dcabe
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-05-27 12:06:45 -0400

    add implicit egress rules

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -45,6 +45,14 @@ resource "aws_security_group" "elb" {
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
 resource "aws_security_group" "ci" {
@@ -84,6 +92,14 @@ resource "aws_security_group" "ci" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
 resource "aws_autoscaling_group" "ci" {
