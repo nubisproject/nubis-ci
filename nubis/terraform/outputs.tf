f3a237580bc99c0f1c06f565e5a9a2680115d4d5
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-06-05 14:19:11 -0400

    publish URLs to Consul

--- nubis/terraform/outputs.tf
+++ nubis/terraform/outputs.tf
@@ -1,3 +1,20 @@
 output "elb" {
     value = "http://ci.${var.domain}/"
 }
+
+
+# Configure the Consul provider
+provider "consul" {
+    address = "ui.${var.region}.consul.${var.environment}.${var.nubis_domain}:80" 
+    datacenter = "${var.region}"
+    scheme = "http"
+}
+
+resource "consul_keys" "jenkins" {
+  key {
+        name = "url"
+        path = "environments/${var.environment}/global/ci/${var.project}"
+        value = "http://ci.${var.domain}/"
+    }
+}
+
