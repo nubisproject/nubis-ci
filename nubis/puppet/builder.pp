5da7fb1e9a7615795d07e29d662675147948cd3d
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-08-31 13:50:52 -0400

    Include nubis-builder v1.0.1 for ssl support

--- nubis/puppet/builder.pp
+++ nubis/puppet/builder.pp
@@ -23,7 +23,7 @@ vcsrepo { "/opt/nubis-builder":
   ensure   => present,
   provider => git,
   source   => 'https://github.com/Nubisproject/nubis-builder.git',
-  revision => "v1.0.0"
+  revision => "v1.0.1"
 }
 
 # XXX: need to move to puppet-packer
