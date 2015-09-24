e9345ffad91d42f9b52a49a7ad88188dc5f04f89
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-09-23 20:44:23 -0400

    install/upgrade awscli (and deps) via PIP. Fixes #107

--- nubis/puppet/builder.pp
+++ nubis/puppet/builder.pp
@@ -1,7 +1,11 @@
 #XXX: Needs released version
 
-package { "awscli":
+# Amazon Linux upgraded python3 and broke boto, so install awscli and dependencies the hard way
+package { "python3-pip":
   ensure => present
+}->
+exec { "upgrade awscli":
+  command => "/usr/bin/pip3 install awscli",
 }
 
 package { "rsync":
