0e52bfb7896118ddac6ee0d63746b06bbe6fb95b
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-12-31 12:40:53 -0500

    Upgrade packer to 0.8.6
    Fixes #151

--- nubis/puppet/builder.pp
+++ nubis/puppet/builder.pp
@@ -28,7 +28,7 @@ vcsrepo { "/opt/nubis-builder":
 
 # XXX: need to move to puppet-packer
 staging::file { 'packer.zip':
-  source => "https://dl.bintray.com/mitchellh/packer/packer_0.8.2_linux_amd64.zip"
+  source => "https://dl.bintray.com/mitchellh/packer/packer_0.8.6_linux_amd64.zip"
 } ->
 staging::extract { 'packer.zip':
   target  => "/usr/local/bin",
