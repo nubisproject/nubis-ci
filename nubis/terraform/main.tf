a9c016cad1060a03e969112859d34b9a12ddd751
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-09-17 19:27:16 -0400

    permissions++

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -425,6 +425,7 @@ resource "aws_iam_role_policy" "ci_deploy" {
                 "elasticloadbalancing:CreateLoadBalancerListeners",
                 "elasticloadbalancing:DescribeLoadBalancerAttributes",
                 "elasticloadbalancing:AddTag",
+                "elasticloadbalancing:AttachLoadBalancerToSubnets",
                 "rds:CreateDBInstance",
                 "rds:CreateDBSubnetGroup",
                 "rds:DeleteDBSubnetGroup",
