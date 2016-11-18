5eccdb600cfda80eb6adbebddcdd35558b14948e
Philippe M. Chiasson <gozer@mozilla.com>
2016-11-18 13:36:37 -0500

    Upgrade to Jenkins 2.19.3 (#339)
    * Upgrade plugins:
     - workflow-scm-step 2.2 => 2.3
     - workflow-job 2.8 => 2.9
    
    Fixes #331
    
    * Upgrade to Jenkins 2.19.3
    
    Fixes #338

--- nubis/puppet/jenkins.pp
+++ nubis/puppet/jenkins.pp
@@ -11,7 +11,7 @@ package { 'daemon':
   ensure => 'present'
 }->
 class { 'jenkins':
-  version            => '2.19.2',
+  version            => '2.19.3',
   configure_firewall => false,
   service_enable     => false,
   service_ensure     => 'stopped',
