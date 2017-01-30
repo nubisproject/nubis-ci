7abca9a1ce8a82cde6f9fbd2ba68788ce66181a4
Philippe M. Chiasson <gozer@mozilla.com>
2017-01-30 11:58:43 -0500

    Make sure we start at the 0-th line when ordering/finding our backup chain (#364)
    * tyop
    
    * add missed EFS IAM permissions
    
    * make sure we start at the 0-th line when ordering/finding our backup chain
    
    Fixes #363

--- nubis/bin/jenkins-startup.sh
+++ nubis/bin/jenkins-startup.sh
@@ -34,7 +34,7 @@ ALL_BACKUPS=$(find $BACKUP_DIR -maxdepth 1 -type d   -name 'FULL*' -o -name 'DIF
 LAST_FULL=$(basename "$(echo "$ALL_BACKUPS" | grep FULL | tail -n1)")
 
 # And all following incrementals
-INCREMENTALS=$(echo "$ALL_BACKUPS" | sed -e "1,/$LAST_FULL/d" | xargs -n1 basename)
+INCREMENTALS=$(echo "$ALL_BACKUPS" | sed -e "0,/$LAST_FULL/d" | xargs -n1 basename)
 
 # Recover from latest backup (full + incrementals)
 for BACKUP in $LAST_FULL $INCREMENTALS; do
