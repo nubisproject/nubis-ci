adf97f17073704c2c92d2a9062d878afc39138b8
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-06-27 11:59:37 -0700

    unneeded security cleanup

--- nubis/puppet/security.pp
+++ nubis/puppet/security.pp
@@ -4,16 +4,3 @@ file { '/var/lib/jenkins/init.groovy.d':
   group  => root,
   mode   => '0755',
 }
-
-file { '/var/lib/jenkins/init.groovy.d/cli-shutdown.groovy':
-  ensure  => file,
-  owner   => root,
-  group   => root,
-  mode    => '0644',
-  source  => 'puppet:///nubis/files/cli-shutdown.groovy',
-
-  require => [
-    File['/var/lib/jenkins/init.groovy.d'],
-    Class['jenkins'],
-  ],
-}
