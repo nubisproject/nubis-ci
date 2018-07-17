21bc4d1ecfea60aa3927a89b6ea38b3de674c2cf
Jason Crowe (Mozilla) <jcrowe@mozilla.com>
2018-07-17 09:46:19 -0400

    Update ci to use nubis-ctl
    - Several Docker fixes
    - Pinned container versions
    - Migrate application Terraform state from Consul to S3
    
    Closes mozilla-itcloud/maintenance-project/issues/11
    Closes mozilla-itcloud/maintenance-project/issues/1
    
    Closes #632
    Closes #256

--- nubis/puppet/docker.pp
+++ nubis/puppet/docker.pp
@@ -1,3 +1,8 @@
+$nubis_json2hcl_version = 'v0.0.2'
+$nubis_jq_version = 'v0.1.0'
+$nubis_builder_version = 'v1.11.0'
+$nubis_deploy_version = 'v1.0.1'
+
 include ::apt
 
 exec { 'inhibit service startup on package installation':
@@ -5,7 +10,8 @@ exec { 'inhibit service startup on package installation':
   command => 'echo exit 101 > /usr/sbin/policy-rc.d; chmod +x /usr/sbin/policy-rc.d',
 }
   -> class { 'docker':
-  proxy => 'http://proxy.service.consul:3128/',
+# Proxy needs to be set up after downloading images
+#  proxy => 'http://proxy.service.consul:3128/',
   bip   => '172.17.42.1/16',
 }
   -> exec { 'un-inhibit service startup on package installation':
@@ -38,4 +44,17 @@ systemd::unit_file { 'docker-cleanup.service':
 ->service { 'docker-cleanup':
   ensure => 'stopped',
   enable => true,
+}
+
+docker::image { "nubisproject/nubis-json2hcl:$nubis_json2hcl_version" : }
+-> docker::image { "nubisproject/nubis-jq:$nubis_jq_version" : }
+-> docker::image { "nubisproject/nubis-builder:$nubis_builder_version": }
+-> docker::image { "nubisproject/nubis-deploy:$nubis_deploy_version": }
+  -> exec { 'alias images for local':
+  path    => ['/sbin','/bin','/usr/sbin','/usr/bin','/usr/local/sbin','/usr/local/bin'],
+  command => "docker tag nubisproject/nubis-json2hcl:$nubis_json2hcl_version nubis-json2hcl; docker tag nubisproject/nubis-jq:$nubis_jq_version nubis-jq; docker tag nubisproject/nubis-builder:$nubis_builder_version nubis-builder; docker tag nubisproject/nubis-deploy:$nubis_deploy_version nubis-deploy",
+}
+  -> exec { 'make docker proxy aware':
+  path    => ['/sbin','/bin','/usr/sbin','/usr/bin','/usr/local/sbin','/usr/local/bin'],
+  command => 'echo \\\nhttp_proxy=\'http://proxy.service.consul:3128/\'\\\nhttps_proxy=\'http://proxy.service.consul:3128/\'\\\n >> /etc/default/docker',
 }
