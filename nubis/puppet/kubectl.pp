25fcba04299cef6a2758562d2f85d5b7b323d787
Jason Crowe <tinnightcap@users.noreply.github.com>
2018-08-01 09:03:25 -0400

    Lint Fixes

--- nubis/puppet/kubectl.pp
+++ nubis/puppet/kubectl.pp
@@ -1,4 +1,4 @@
-$apt_repo_url = "http://apt.kubernetes.io/"
+$apt_repo_url = 'http://apt.kubernetes.io/'
 
 include ::apt
 
@@ -10,9 +10,9 @@ apt::source { 'kubectl':
     id     => '54A647F9048D5688D7DA2ABE6A030B21BA07F4FB',
     source => 'https://packages.cloud.google.com/apt/doc/apt-key.gpg'
   },
-  include   =>  {
+  include  =>  {
     src => false,
- },
+  },
 }
 
 package { 'kubectl':
